56da82e05a7cfe9f325b92d077b7b4541acf9887
seifmhijras <seifmhijras@gmail.com>
2025-11-07 02:16:01 +0200

    Add installation script for AAlib package
    This script installs AAlib by checking for an existing folder, downloading the package, configuring, building, and installing it. It also makes several modifications to source files before the installation process.

---
+++ scripts/AAlib-1.4rc5.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# set -E
+# trap 'echo "❌ Error: command failed at line $LINENO"; exit 1' ERR
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh <SCRIPT>
+    echo "✅ the package downloaded successfully"
+    sed -i -e '/AM_PATH_AALIB,/s/AM_PATH_AALIB/[&]/' aalib.m4
+    sed -e 's/8x13bold/-*-luxi mono-bold-r-normal--13-120-*-*-m-*-*-*/' \
+    -i src/aax.c
+    sed 's/stdscr->_max\([xy]\) + 1/getmax\1(stdscr)/' \
+    -i src/aacurses.c
+    sed -i '1i#include <stdlib.h>'                            \
+    src/aa{fire,info,lib,linuxkbd,savefont,test,regist}.c &&
+    sed -i '1i#include <string.h>'                            \
+    src/aa{kbdreg,moureg,test,regist}.c                   &&
+    sed -i '/X11_KBDDRIVER/a#include <X11/Xutil.h>'           \
+    src/aaxkbd.c                                          &&
+    sed -i '/rawmode_init/,/^}/s/return;/return 0;/'          \
+    src/aalinuxkbd.c                                      &&
+    autoconf
+    ./configure --prefix=/usr             \
+            --infodir=/usr/share/info \
+            --mandir=/usr/share/man   \
+            --with-ncurses=/usr       \
+            --disable-static          
+    echo "✅ the package configured successfully"
+    
+    make
+    echo "✅ the package made successfully"
+    
+    make install
+    echo "✅ the package installed successfully"
+    
+fi
+
+echo "🎉 FINISHED :)"
