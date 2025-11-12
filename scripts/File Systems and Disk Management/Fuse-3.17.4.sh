0a588d160aecfc7d1f1906f15b6bfaba5093bc73
mohamed <elsharifmohamed577@gmail.com>
2025-11-12 03:28:44 +0200

    Add scripts for Fuse-3.17.4 and LZO-2.10 package management

---
+++ scripts/File Systems and Disk Management/Fuse-3.17.4.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# set -E
+# trap 'echo "❌ Error: command failed at line $LINENO"; exit 1' ERR
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh  https://github.com/libfuse/libfuse/releases/download/fuse-3.17.4/fuse-3.17.4.tar.gz
+    echo "✅ the package downloaded successfully"
+
+    sed -i '/^udev/,$ s/^/#/' util/meson.build &&
+
+    mkdir build &&
+    cd    build
+
+   echo "🔧 Running configure..."
+    if ! meson setup --prefix=/usr --buildtype=release .. ; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! ninja; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! ninja install   ; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+    chmod u+s /usr/bin/fusermount3 &&
+
+    cd ..                          &&
+    cp -Rv doc/html -T /usr/share/doc/fuse-3.17.4 &&
+    install -v -m644   doc/{README.NFS,kernel.txt} \
+                    /usr/share/doc/fuse-3.17.4
+
+fi
+
+
+echo "🎉 FINISHED :)"
+```
---
+++ scripts/File Systems and Disk Management/dosfstools-4.2.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# set -E
+# trap 'echo "❌ Error: command failed at line $LINENO"; exit 1' ERR
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh  https://github.com/dosfstools/dosfstools/releases/download/v4.2/dosfstools-4.2.tar.gz
+    echo "✅ the package downloaded successfully"
+
+    
+
+   echo "🔧 Running configure..."
+    if ! ./configure --prefix=/usr            \
+            --enable-compat-symlinks \
+            --mandir=/usr/share/man  \
+            --docdir=/usr/share/doc/dosfstools-4.2 ; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! make; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! make install; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+    
+
+fi
+
+
+echo "🎉 FINISHED :)"
+```
