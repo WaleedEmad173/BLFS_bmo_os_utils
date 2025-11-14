64c720bb27b56b3385272f6d717e3db2df80b7e4
abdou <abdouashrat123@gmail.com>
2025-11-14 20:00:07 +0200

    Add script for downloading and building gmmlib-22.8.2

---
+++ scripts/General Libraries/gmmlib-22.8.2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh https://github.com/lfs-book/gmmlib/archive/v22.8.2/gmmlib-22.8.2.tar.gz
+    echo "✅ the package downloaded successfully"
+
+   # <MORE_COMMAND_IF_EXISTS_WITH_IF_STATEMENT>
+    mkdir build &&
+    cd    build 
+
+    echo "🔧 Running configure..."
+    if ! cmake -D CMAKE_INSTALL_PREFIX=/usr \
+      -D BUILD_TYPE=Release        \
+      -G Ninja                     \
+      -W no-dev ..        ; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! ninja; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! ninja install; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+   # <ETC>
+
+fi
+
+
+echo "🎉 FINISHED :)"
