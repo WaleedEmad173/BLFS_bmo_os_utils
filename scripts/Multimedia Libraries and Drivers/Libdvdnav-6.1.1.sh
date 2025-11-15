d96b7410f2a3308641bbf667675eab7374c6870e
omargaber <omarashraf11100@gmail.com>
2025-11-15 05:39:38 +0200

    add libdvdnav-6.1.1 installation script

---
+++ scripts/Multimedia Libraries and Drivers/Libdvdnav-6.1.1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash   
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+
+    # wget <LINK> --no-check-certificate
+    
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh  https://get.videolan.org/libdvdnav/6.1.1/libdvdnav-6.1.1.tar.bz2
+    echo "✅ the package downloaded successfully"
+
+    # <MORE_COMMAND_IF_EXISTS_WITH_IF_STATEMENT>
+
+   echo "🔧 Running configure..."
+    if ! ./configure --prefix=/usr    \
+            --disable-static \
+            --docdir=/usr/share/doc/libdvdnav-6.1.1; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! make; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! make install; then
+        echo "❌ Error: make failed!"
+        exit 1nstal
+    fi
+
+   # <ETC>
+
+fi
+
+
+echo "🎉 FINISHED :)"
