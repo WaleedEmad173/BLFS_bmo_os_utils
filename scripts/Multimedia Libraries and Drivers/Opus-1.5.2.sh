e6368c769c56b8180ca075d383cda50275d397aa
omargaber <omarashraf11100@gmail.com>
2025-11-21 01:05:07 +0200

    Add installation script for Opus version 1.5.2

---
+++ scripts/Multimedia Libraries and Drivers/Opus-1.5.2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash         
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+
+    # wget <LINK> --no-check-certificate
+    
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh https://downloads.xiph.org/releases/opus/opus-1.5.2.tar.gz
+    echo "✅ the package downloaded successfully"
+
+    mkdir build 
+    cd    build 
+
+
+   echo "🔧 Running configure..."
+    if ! meson setup --prefix=/usr        \
+                     --buildtype=release  \
+                     -D docdir=/usr/share/doc/opus-1.5.2; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! ninja; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! ninja install; then
+        echo "❌ Error: make failed!"
+        exit 1nstal
+    fi
+
+   # <ETC>
+
+fi
+
+
+echo "🎉 FINISHED :)"
