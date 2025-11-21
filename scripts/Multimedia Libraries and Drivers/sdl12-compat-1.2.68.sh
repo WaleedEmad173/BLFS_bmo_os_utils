39ddce6de5a165c94c60a8dc8029bc4494125cc0
omargaber <omarashraf11100@gmail.com>
2025-11-21 02:03:24 +0200

    Add installation script for SDL12 compatibility version 1.2.68

---
+++ scripts/Multimedia Libraries and Drivers/sdl12-compat-1.2.68.sh
@@ -0,0 +1,48 @@
+#!/bin/bash  
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+
+   # wget <LINK> --no-check-certificate
+
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh https://github.com/libsdl-org/sdl12-compat/archive/release-1.2.68/sdl12-compat-release-1.2.68.tar.gz
+    echo "✅ the package downloaded successfully"
+
+   mkdir build 
+   cd    build 
+
+   echo "🔧 Running configure..."
+    if ! cmake -D CMAKE_INSTALL_PREFIX=/usr \
+               -D CMAKE_BUILD_TYPE=RELEASE  \
+               ..; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! make; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! make install; then
+        echo "❌ Error: make failed!"
+        exit 1nstal
+    fi
+
+    rm -vf /usr/lib/libSDLmain.a
+
+fi
+
+
+echo "🎉 FINISHED :)"
