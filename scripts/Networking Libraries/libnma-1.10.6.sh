7611ebdabafc3aa7d9c9eff051ce020150c5fb00
WaleedEmad173 <waleed.emad173@gmail.com>
2025-11-25 22:23:43 +0200

    Add libnma installation script version 1.10.6 with build and installation steps

---
+++ scripts/Networking Libraries/libnma-1.10.6.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh https://download.gnome.org/sources/libnma/1.10/libnma-1.10.6.tar.xz
+    echo "✅ the package downloaded successfully"
+
+   # <MORE_COMMAND_IF_EXISTS_WITH_IF_STATEMENT>
+   mkdir build 
+   cd    build
+
+
+   echo "🔧 Running configure..."
+    if ! meson setup ..            \
+      --prefix=/usr       \
+      --buildtype=release \
+      -D gtk_doc=false    \
+      -D libnma_gtk4=true \
+      -D mobile_broadband_provider_info=false ; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! ninja; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! ninja install; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+   # <ETC>
+
+fi
+
+
+echo "🎉 FINISHED :)"
