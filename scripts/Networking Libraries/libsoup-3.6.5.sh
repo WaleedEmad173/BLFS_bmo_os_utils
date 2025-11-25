6174394473f59ef5c16762b66dfb99e85db1ee07
WaleedEmad173 <waleed.emad173@gmail.com>
2025-11-25 22:40:30 +0200

    Add libsoup installation script version 3.6.5 with build and installation steps

---
+++ scripts/Networking Libraries/libsoup-3.6.5.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh  https://download.gnome.org/sources/libsoup/3.6/libsoup-3.6.5.tar.xz
+    echo "✅ the package downloaded successfully"
+
+   # <MORE_COMMAND_IF_EXISTS_WITH_IF_STATEMENT>
+   sed 's/apiversion/soup_version/' -i docs/reference/meson.build
+   mkdir build 
+   cd    build 
+
+   echo "🔧 Running configure..."
+    if ! meson setup --prefix=/usr          \
+            --buildtype=release    \
+            --wrap-mode=nofallback \
+            ..                     ; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! ninja; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! ninja install; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+   # <ETC>
+
+fi
+
+
+echo "🎉 FINISHED :)"
