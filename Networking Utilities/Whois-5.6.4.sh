f85919b614a5458e5809541e199c0a270012cef9
WaleedEmad173 <waleed.emad173@gmail.com>
2025-11-25 20:29:35 +0200

    Add Whois installation script version 5.6.4 with build and installation steps

---
+++ Networking Utilities/Whois-5.6.4.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh  https://github.com/rfc1036/whois/archive/v5.6.4/whois-5.6.4.tar.gz
+    echo "✅ the package downloaded successfully"
+
+   # <MORE_COMMAND_IF_EXISTS_WITH_IF_STATEMENT>
+
+   echo "🔧 Running configure..."
+    if ! <CONFIG>; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! make; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! make prefix=/usr install-whois&
+        make prefix=/usr install-mkpasswd&
+        make prefix=/usr install-pos; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+   # <ETC>
+
+fi
+
+
+echo "🎉 FINISHED :)"
