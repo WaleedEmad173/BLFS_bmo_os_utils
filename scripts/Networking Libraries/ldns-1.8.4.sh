b938418f05ca6bd05022039a425ea1d07975d8f8
WaleedEmad173 <waleed.emad173@gmail.com>
2025-11-25 21:56:45 +0200

    Add ldns installation script version 1.8.4 with build and installation steps

---
+++ scripts/Networking Libraries/ldns-1.8.4.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh  https://www.nlnetlabs.nl/downloads/ldns/ldns-1.8.4.tar.gz
+    echo "✅ the package downloaded successfully"
+
+   # <MORE_COMMAND_IF_EXISTS_WITH_IF_STATEMENT>
+
+   echo "🔧 Running configure..."
+    if ! ./configure --prefix=/usr     \
+            --sysconfdir=/etc \
+            --disable-static  \
+            --with-drill      ; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! make; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+    make doc
+    
+    echo "⚙️ installing..."
+    if ! make install; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+   # <ETC>
+   install -v -m755 -d /usr/share/doc/ldns-1.8.4 &&
+   install -v -m644 doc/html/* /usr/share/doc/ldns-1.8.4
+
+fi
+
+
+echo "🎉 FINISHED :)"
